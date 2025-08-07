354118cc468deefae18c75cf67b4a9be68cade74
Lueblum <chaikeaittham_s@su.ac.th>
2025-08-07 16:26:04 +0000

    lab-week4

---
+++ phai-app/src/Goodbye.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Goodbye(){
+    return (
+        <div>
+            <p>Goodbye, This is a new Component!</p>
+        </div>
+    );
+}
+
+export default Goodbye;
